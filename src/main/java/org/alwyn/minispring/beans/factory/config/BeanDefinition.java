8561dc79282bccfae5175dbc30aa632a9f74cbcf
AlwynTan <2660412180@qq.com>
2024-10-11 14:34:18 +0800

    add: PropertyValues for BeanDefinition

--- src/main/java/org/alwyn/minispring/beans/factory/config/BeanDefinition.java
+++ src/main/java/org/alwyn/minispring/beans/factory/config/BeanDefinition.java
@@ -1,9 +1,18 @@
 package org.alwyn.minispring.beans.factory.config;
 
+import org.alwyn.minispring.beans.PropertyValues;
+
 public class BeanDefinition {
     private Class beanClass;
 
+    private PropertyValues propertyValues;
+
     public BeanDefinition(Class beanClass){this.beanClass = beanClass;}
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues;
+    }
 
     public Class getBeanClass() {
         return beanClass;
@@ -12,4 +21,9 @@ public class BeanDefinition {
     public void setBeanClass(Class beanClass) {
         this.beanClass = beanClass;
     }
+
+    public PropertyValues getPropertyValues(){return propertyValues;}
+
+    public void setPropertyValues(PropertyValues propertyValues) {this.propertyValues = propertyValues;}
+
 }
