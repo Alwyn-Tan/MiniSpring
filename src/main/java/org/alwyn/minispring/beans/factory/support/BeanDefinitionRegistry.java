c97b2f5d89773e6d5b95bae193a8107d2d9442d8
AlwynTan <2660412180@qq.com>
2024-10-16 15:34:20 +0800

    add: check for BeanDefinition in Registry to avoid duplicate of Bean

--- src/main/java/org/alwyn/minispring/beans/factory/support/BeanDefinitionRegistry.java
+++ src/main/java/org/alwyn/minispring/beans/factory/support/BeanDefinitionRegistry.java
@@ -1,7 +1,13 @@
 package org.alwyn.minispring.beans.factory.support;
 
+import org.alwyn.minispring.beans.BeansException;
 import org.alwyn.minispring.beans.factory.config.BeanDefinition;
 
 public interface BeanDefinitionRegistry {
     void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
+
+    BeanDefinition getBeanDefinition(String beanName) throws BeansException;
+
+    boolean containsBeanDefinition(String beanName);
+
 }
