2008382b52a9f72f8c224f76c6e78b0d18e36011
AlwynTan <2660412180@qq.com>
2024-11-10 23:24:46 +0800

    upgrade: 完善ListableBeanFactory的功能,增加containsBeanDefinition,getBeanDefinitionCount等方法

--- src/main/java/org/alwyn/minispring/beans/factory/ListableBeanFactory.java
+++ src/main/java/org/alwyn/minispring/beans/factory/ListableBeanFactory.java
@@ -2,10 +2,25 @@ package org.alwyn.minispring.beans.factory;
 
 import org.alwyn.minispring.beans.BeansException;
 
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Just like {@link  java.util.List#isEmpty()} ,{@link java.util.List#size()} and {@link List#iterator()}, a Listable
+ * class should provide similar methods. {@link ListableBeanFactory} provides {@link #containsBeanDefinition(String)},
+ * {@link #getBeanDefinitionCount()} and {@link #getBeanDefinitionNames()}, used to enumerate all the bean instances,
+ * rather than looking up the bean by name.
+ */
+
 public interface ListableBeanFactory extends BeanFactory {
-    <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException;
+
+    boolean containsBeanDefinition(String beanName);
+
+    int getBeanDefinitionCount();
 
     String[] getBeanDefinitionNames();
+
+    String[] getBeanNamesForType(Class<?> type);
+
+    <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException;
 }
